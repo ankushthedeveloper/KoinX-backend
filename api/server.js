415e736161bf2f6ac0a5bee2a67c940819b9d239
ankushthedeveloper <ankushbokra@gmail.com>
2025-01-14 14:42:31 +0530

    Changes in vercel.json to solve the Timeout Error(504)

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import statsRouter from './routes/stats.js';
-import deviationRouter from './routes/deviation.js';
-import backgroundJob from './Services/backgroundJob.js';
-import fetchCryptoData from './Services/backgroundJob.js';
-import connectmongodb from './utils/dbconnect.js';
+import statsRouter from '../routes/stats.js';
+import deviationRouter from '../routes/deviation.js';
+import backgroundJob from '../Services/backgroundJob.js';
+import fetchCryptoData from '../Services/backgroundJob.js';
+import connectmongodb from '../utils/dbconnect.js';
 dotenv.config();
 
 const app = express();
